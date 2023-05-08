5a0aa6f4b24f547c168d3b2fc069cfadd5443f52
SAMEER YALLA <86701298+sameeryalla@users.noreply.github.com>
2023-05-08 18:44:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,25 @@ provider "aws"{
    access_key="AKIAXKYQSQ7TVBWP5J3Y"
    secret_key="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" 
 }
+
+provider "linode" {
+
+   #token = ""  
+   
+}
+
+resource "aws_vpc" "vpc"{
+   cidr_block = "10.0.0.0/16"
+   tags{
+      name="dev-vpc"
+   }
+}
+
+resource "aws_subnet" "dev-subnet-1"{
+   depends_on = [ aws_vpc.vpc ]
+   vpc_id = aws_vpc.vpc.id
+   cidr_block = "10.0.0.0/16"
+   tags{
+      name="dev-subnet-1"
+   }
+}
